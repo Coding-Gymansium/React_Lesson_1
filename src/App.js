ddd0425897f6ef1165c1154433411c565e721399
Nico Rithner <nicorithner@gmail.com>
2021-02-13 17:24:19 -0700

    Complete up to lesson 27

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-				constructor() {
-								super();
+class App extends React.Component 
+{
+	constructor() {
+		super();
 
-								this.state = {
-												monsters: [
-																{
-																				name: 'Frankestein'
-																},
-																{
-																				name: 'Dracula'
-																},
-																{
-																				name: 'Zombie'
-																},
-												]
-								};
+		this.state = {
+			monsters: [
+				{
+					name: 'Frankestein',
+					id: 'asc1'
+				},
+				{
+					name: 'Dracula',
+					id: 'asc2'
+				},
+				{
+					name: 'Zombie',
+					id: 'asc3'
 				}
+			]
+		};
+	}
 
-				render() {
-					 return (
- 					   <div className="App">
- 					     <header className="App-header">
- 					       <img src={logo} className="App-logo" alt="logo" />
- 					       <p>
- 					         Edit <code>src/App.js</code> and save to reload.
- 					       </p>
- 					       <a
- 					         className="App-link"
- 					         href="https://reactjs.org"
- 					         target="_blank"
- 					         rel="noopener noreferrer"
- 					       >
- 					         Learn React
- 					       </a>
- 					     </header>
- 					   </div>
- 					 );
-				}					
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({ monsters: users }))
+	}
+
+	render() {
+		return (
+			<div className="App">
+				{
+					this.state.monsters.map(monster => (
+						<h1 key={monster.id}> { monster.name} </h1>
+						))}
+			</div>
+		);
+	}
 };
 export default App;
